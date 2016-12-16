654932d214987f515d677f25d2e82aa6022b1690
Thuan Nguyen <thuannguyen@thuans-macbook-pro.local>
2016-12-16 09:56:47 -0800

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Thuan Nguyen on 12/16/16.
+//  Copyright © 2016 Thuan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
