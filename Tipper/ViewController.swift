8ac00c99d39e583c3982aeaced0eaf07a9bb1210
Thuan Nguyen <thuannguyen@Thuans-MacBook-Pro.local>
2016-12-18 10:43:25 -0800

    Adding .gitignore and .gitattributes files

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func tipChange(_ sender: Any) {
+        calculate(0)
+    }
+    @IBAction func calculate(_ sender: Any) {
+        let tipPercent = [0.15,0.18,0.21]
+        let bill = (Double)(billLabel.text!) ?? 0
+        let tip = bill*tipPercent[tipSegment.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip )
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+    
 
 }
 
